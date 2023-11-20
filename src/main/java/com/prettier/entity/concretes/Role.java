87aa28557918fbc905ccadbeb24456749517715c
Ramiz Hasan <ramizhasan2929@gmail.com>
2023-11-20 22:14:24 +0300

    Role entity and RoleType enum have done

---
+++ src/main/java/com/prettier/entity/concretes/Role.java
@@ -0,0 +1,35 @@
+package com.prettier.entity.concretes;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.prettier.entity.enums.RoleType;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+@SuperBuilder
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role_name", nullable = false)
+    private RoleType roleType;
+
+
+    // Entity Relations
+//    @JsonIgnore
+//   @ManyToMany(mappedBy = "roleSet", fetch = FetchType.EAGER)
+//    private Set<User> userSet;
+
+}
