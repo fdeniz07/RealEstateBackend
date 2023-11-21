0bac409a6152f1dc23c10d62fd11e185e5614f66
Ramiz Hasan <ramizhasan2929@gmail.com>
2023-11-21 18:56:06 +0300

    CategoryPropertyValue created. Relations are taken into comment.

---
+++ src/main/java/com/prettier/entity/concretes/CategoryPropertyValue.java
@@ -0,0 +1,36 @@
+package com.prettier.entity.concretes;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Entity
+@Table(name = "category_property_value")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class CategoryPropertyValue {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "value", nullable = false)
+    @Size(max = 100)
+    private String value;
+
+    //RELATION -----------------------------------------------------------------
+//Realtion with Parent "category_property-keys"
+//    @ManyToOne
+//    @JoinColumn(name = "category_property_key_id")
+//    private CategoryPropertyKey categoryPropertyKey;
+
+    // Relation with Parent Adverts table
+//    @ManyToOne
+//    @JoinColumn(name = "advert_id")
+//    private Advert advert;
+
+
+}
