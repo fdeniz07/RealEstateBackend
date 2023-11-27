5d5177434d28af898db9d431f44446c01d216923
Fatih Deniz <deniz.fatih@hotmail.de>
2023-11-27 22:00:06 +0100

    Rev1

--- src/main/java/com/prettier/service/ContactService.java
+++ src/main/java/com/prettier/service/ContactService.java
@@ -7,7 +7,6 @@ import com.prettier.payload.response.ContactResponse;
 import com.prettier.repository.ContactRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 @Service
