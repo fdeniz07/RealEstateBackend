cefaf0f164466c017322fe823e48b23e567d2c6e
Ahmet-SAAT <avahmetsaat@gmail.com>
2023-11-27 20:41:25 +0300

    simple fix

--- src/main/java/com/prettier/controller/CategoryController.java
+++ src/main/java/com/prettier/controller/CategoryController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("category")
+@RequestMapping("categories")
 public class CategoryController {
     private final CategoryService categoryService;
 }
