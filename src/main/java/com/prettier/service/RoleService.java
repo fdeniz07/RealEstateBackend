f201d329711a17f30682829d9558b51d8f5534dc
Ramiz Hasan <ramizhasan2929@gmail.com>
2023-11-27 20:24:03 +0300

    RoleService and Role Repository created and connected

---
+++ src/main/java/com/prettier/service/RoleService.java
@@ -0,0 +1,12 @@
+package com.prettier.service;
+
+import com.prettier.repository.RoleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RoleService {
+    private final RoleRepository roleRepository;
+
+}
