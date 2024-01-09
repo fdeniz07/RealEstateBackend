d829a77a2847103101f9386857761c1de8ec9fcd
Fatih Deniz <deniz.fatih@hotmail.de>
2024-01-09 22:38:18 +0100

    Contact Message has been updated

--- src/main/java/com/prettier/payload/mapper/ContactMapper.java
+++ src/main/java/com/prettier/payload/mapper/ContactMapper.java
@@ -18,18 +18,4 @@ public interface ContactMapper {
     ContactResponse toResponse(Contact contact);
 
     Contact toUpdatedContact(ContactUpdateRequest contactUpdateRequest, Contact existingContact);
-    
-    
-//    @Autowired
-//    private ModelMapper modelMapper;
-//
-//    public Contact toContact(ContactRequest contactRequest) {
-//
-//        return modelMapper.map(contactRequest, Contact.class);
-//    }
-//
-//    public ContactResponse toResponse(Contact contact) {
-//
-//        return modelMapper.map(contact, ContactResponse.class);
-//    }
 }
