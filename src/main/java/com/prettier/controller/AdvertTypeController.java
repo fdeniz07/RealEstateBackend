94b7ffbd1dc9c53cbc7fd64397a83098b74da4f1
Fatih Deniz <deniz.fatih@hotmail.de>
2025-03-01 23:28:34 +0100

    Added Javadoc documentation on AdvertTypeController

--- src/main/java/com/prettier/controller/AdvertTypeController.java
+++ src/main/java/com/prettier/controller/AdvertTypeController.java
@@ -8,6 +8,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * REST controller for managing advertisement types in the system.
+ * Handles endpoints for advertisement type operations.
+ *
+ * <p>This controller manages the different types of advertisements that can be created
+ * in the real estate system.</p>
+ *
+ * @author Fatih Deniz
+ * @version 1.0
+ */
 @RestController
 @RequiredArgsConstructor
 @Tag(name = "Advert-Type", description = "Prettier Homes - Real Estate APIs") //Swagger dökümani icin
