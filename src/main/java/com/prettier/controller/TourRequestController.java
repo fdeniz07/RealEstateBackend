f24c4c2cd25d4179e50336f6a98cabd6dab3885c
Fatih Deniz <deniz.fatih@hotmail.de>
2025-03-12 21:59:06 +0100

    Added Javadoc documentation on TourRequestController

--- src/main/java/com/prettier/controller/TourRequestController.java
+++ src/main/java/com/prettier/controller/TourRequestController.java
@@ -9,6 +9,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * REST controller for managing property tour requests in the system.
+ * Handles endpoints for creating, retrieving, updating, and managing property tour requests.
+ *
+ * <p>This controller provides functionality for users to request property tours
+ * and for administrators to manage these requests.</p>
+ *
+ * @author Fatih Deniz
+ * @version 1.0
+ */
 @RestController
 @RequiredArgsConstructor
 @Tag(name = "Tour-Request", description = "Prettier Homes - Real Estate APIs") //Swagger dökümani icin
